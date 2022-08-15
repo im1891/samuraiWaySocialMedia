7a7d09470a7edecbf20a9b2419a7d3e678116b17
Igor Muratov <im1891@gmail.com>
2022-08-15 10:07:52 +0300

    Created components Header and Technologies

---
+++ src/components/Technologies.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Technologies() {
+    return <div>
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+        </ul>
+    </div>
+}
+
+export default Technologies
